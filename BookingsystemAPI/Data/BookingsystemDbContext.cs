4f261bb30c087d90437f9bbf7c5da72c6ad19c76
Daniel Johansson <dannesnake@gmail.com>
2024-05-07 18:01:07 +0200

    Add seed data to Database

--- BookingsystemAPI/Data/BookingsystemDbContext.cs
+++ BookingsystemAPI/Data/BookingsystemDbContext.cs
@@ -27,6 +27,24 @@ namespace BookingsystemAPI.Data
                 PhoneNumber = "1234567890",
             });
 
+            modelBuilder.Entity<Customer>().HasData(new Customer
+            {
+                CustomerId = 2,
+                FirstName = "Tobias",
+                LastName = "Johansson",
+                EmailAddress = "tobias@johansson.se",
+                PhoneNumber = "1234567890",
+            });
+
+            modelBuilder.Entity<Customer>().HasData(new Customer
+            {
+                CustomerId = 3,
+                FirstName = "Markus",
+                LastName = "Johansson",
+                EmailAddress = "markus@johansson.se",
+                PhoneNumber = "1234567890",
+            });
+
             modelBuilder.Entity<Company>().HasData(new Company 
             { 
                 CompanyId = 1,
@@ -40,6 +58,51 @@ namespace BookingsystemAPI.Data
                 AppointmentEnd = new DateTime(2024,06,16,10,30,00),
                 CustomerId = 1,
                 CompanyId=1,
+            });
+
+            modelBuilder.Entity<Appointment>().HasData(new Appointment
+            {
+                AppointmentId = 2,
+                AppointmentStart = new DateTime(2024, 08, 01, 12, 00, 00),
+                AppointmentEnd = new DateTime(2024, 08, 01, 13, 00, 00),
+                CustomerId = 1,
+                CompanyId = 1,
+            });
+
+            modelBuilder.Entity<Appointment>().HasData(new Appointment
+            {
+                AppointmentId = 3,
+                AppointmentStart = new DateTime(2024, 08, 20, 07, 15, 00),
+                AppointmentEnd = new DateTime(2024, 08, 21, 06, 59, 00),
+                CustomerId = 1,
+                CompanyId = 1,
+            });
+
+            modelBuilder.Entity<Appointment>().HasData(new Appointment
+            {
+                AppointmentId = 4,
+                AppointmentStart = new DateTime(2024, 05, 19, 15, 30, 00),
+                AppointmentEnd = new DateTime(2024, 05, 19, 16, 00, 00),
+                CustomerId = 2,
+                CompanyId = 1,
+            });
+
+            modelBuilder.Entity<Appointment>().HasData(new Appointment
+            {
+                AppointmentId = 5,
+                AppointmentStart = new DateTime(2024, 06, 01, 08, 20, 00),
+                AppointmentEnd = new DateTime(2024, 06, 01, 11, 45, 00),
+                CustomerId = 2,
+                CompanyId = 1,
+            });
+
+            modelBuilder.Entity<Appointment>().HasData(new Appointment
+            {
+                AppointmentId = 6,
+                AppointmentStart = new DateTime(2024, 07, 09, 12, 50, 00),
+                AppointmentEnd = new DateTime(2024, 07, 09, 15, 45, 00),
+                CustomerId = 3,
+                CompanyId = 1,
             });
         }
     }
